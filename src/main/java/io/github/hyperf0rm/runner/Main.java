db49655699e62c341f87880af3349680b7b1986f
hyperf0rm <drk2701@gmail.com>
2026-08-21 09:48:41 +0300

    build: update configuration

---
+++ src/main/java/io/github/hyperf0rm/runner/Main.java
@@ -0,0 +1,18 @@
+package io.github.hyperf0rm.runner;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public void start(Stage stage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
