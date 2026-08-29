c1a7b250d68384ce66e13aab04c153fb7ddce502
hyperf0rm <drk2701@gmail.com>
2026-08-29 19:36:20 +0300

    feat: make ui theme dark

--- src/main/java/io/github/hyperf0rm/runner/Main.java
+++ src/main/java/io/github/hyperf0rm/runner/Main.java
@@ -1,5 +1,6 @@
 package io.github.hyperf0rm.runner;
 
+import atlantafx.base.theme.NordDark;
 import io.github.hyperf0rm.runner.ui.MainView;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -9,6 +10,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     public void start(Stage stage) {
+        Application.setUserAgentStylesheet(new NordDark().getUserAgentStylesheet());
         Scene scene = new Scene(new MainView());
         stage.setScene(scene);
         stage.setMaximized(true);
