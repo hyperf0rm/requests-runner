3512d1e382c9e53a519bbe7eed6530aeea023c17
hyperf0rm <drk2701@gmail.com>
2026-09-04 23:58:13 +0300

    feat: add view for decoder-encoder tools

---
+++ src/main/java/io/github/hyperf0rm/runner/ui/tools/DecoderEncoderView.java
@@ -0,0 +1,64 @@
+package io.github.hyperf0rm.runner.ui.tools;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.*;
+
+public class DecoderEncoderView extends BorderPane {
+
+    private final TextArea inputTextArea = new TextArea();
+    private final TextArea outputTextArea = new TextArea();
+    private final Button decodeButton = new Button("Decode");
+    private final Button encodeButton = new Button("Encode");
+
+    public DecoderEncoderView() {
+
+        VBox inputVBox = createVBoxContainer("Input:", inputTextArea, true);
+        VBox outputVBox = createVBoxContainer("Output:", outputTextArea, false);
+
+        GridPane gridPane = createGrid();
+        gridPane.add(inputVBox, 0, 0);
+        gridPane.add(outputVBox, 1, 0);
+
+        HBox actionBar = new HBox();
+        actionBar.setSpacing(10);
+        actionBar.setPadding(new Insets(10, 10, 0, 10));
+        actionBar.setAlignment(Pos.CENTER);
+        actionBar.getChildren().addAll(decodeButton, encodeButton);
+        setTop(actionBar);
+        setCenter(gridPane);
+        setMargin(gridPane, new Insets(10));
+    }
+
+    private VBox createVBoxContainer(String labelName, TextArea textArea, boolean isEditable) {
+        Label label = new Label(labelName);
+        VBox container = new VBox(label, textArea);
+        VBox.setVgrow(textArea, Priority.ALWAYS);
+        textArea.setEditable(isEditable);
+        textArea.setWrapText(true);
+        return container;
+    }
+
+    private GridPane createGrid() {
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+
+        ColumnConstraints leftCol = new ColumnConstraints();
+        leftCol.setPercentWidth(50.0);
+        leftCol.setHgrow(Priority.ALWAYS);
+        ColumnConstraints rightCol = new ColumnConstraints();
+        rightCol.setPercentWidth(50.0);
+        rightCol.setHgrow(Priority.ALWAYS);
+        gridPane.getColumnConstraints().addAll(leftCol, rightCol);
+
+        RowConstraints rowConstraints = new RowConstraints();
+        rowConstraints.setVgrow(Priority.ALWAYS);
+        gridPane.getRowConstraints().add(rowConstraints);
+
+        return gridPane;
+    }
+
+}
--- src/main/java/io/github/hyperf0rm/runner/ui/tools/ToolsView.java
+++ src/main/java/io/github/hyperf0rm/runner/ui/tools/ToolsView.java
@@ -1,8 +1,8 @@
 package io.github.hyperf0rm.runner.ui.tools;
 
 import javafx.application.Platform;
-import javafx.geometry.Pos;
 import javafx.geometry.Side;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
@@ -11,6 +11,7 @@ import javafx.scene.control.TabPane;
 public class ToolsView extends TabPane {
 
     private static final double TAB_WIDTH = 150.0;
+    private final DecoderEncoderView urlDecoderView = new DecoderEncoderView();
 
     public ToolsView() {
         this.setSide(Side.LEFT);
@@ -19,15 +20,15 @@ public class ToolsView extends TabPane {
         this.setTabMaxHeight(TAB_WIDTH);
         this.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
         this.getStyleClass().add("horizontal-tab-pane");
-        Tab urlDecoderTab = createTab("URL Decoder");
-        Tab unicodeDecoderTab = createTab("Unicode Decoder");
-        this.getTabs().addAll(urlDecoderTab, unicodeDecoderTab);
-
+        Tab urlDecoderTab = createTab("URL Decoder", urlDecoderView);
+        //Tab unicodeDecoderTab = createTab("Unicode Decoder", urlDecoderView);
+        this.getTabs().addAll(urlDecoderTab);
     }
 
-    public Tab createTab(String labelName) {
+    public Tab createTab(String labelName, Node content) {
         Label label = new Label(labelName);
         Tab tab = new Tab();
+        tab.setContent(content);
         tab.setGraphic(label);
         Platform.runLater(() -> {
             Parent tabContainer = tab.getGraphic().getParent().getParent();
