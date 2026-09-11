fc60769c59f1b4a4087da1dbd9ecdaa4256167bd
hyperf0rm <drk2701@gmail.com>
2026-09-11 18:33:42 +0300

    feat: add model for notes

---
+++ src/main/java/io/github/hyperf0rm/runner/model/Note.java
@@ -0,0 +1,33 @@
+package io.github.hyperf0rm.runner.model;
+
+public class Note {
+
+    private String name;
+    private String text;
+
+    public Note(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
